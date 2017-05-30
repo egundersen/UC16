e740fdce27fd75c089fc4941a69f868200d9b3d1
Erikdev3rd <erikgundersen.200@Gmail.com>
2017-05-30 13:21:20 -0700

    Deleted eveything and replaced it with the Holiday API

--- UC16.js
+++ UC16.js
@@ -1,21 +1,28 @@
 /**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
+ * Uses AJAX to query an internet data source for myCountry codes
+ * @param {string} country The element id that has the myCountry code
+ * @param {string} year The element id that has the myYear code
+ * @param {string} month The element id that has the myMonth code
  */
-function findZip(zipId) {
-    // First get the zip code from the HTML textbox
-    var zip = document.getElementById(zipId).value;
+function getResponse(country, year, month) {
+    // First get the myCountry code from the HTML textbox
+    var myCountry = document.getElementById(country).value;
+    var myYear = document.getElementById(year).value;
+    var myMonth = document.getElementById(month).value;
     // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
+            console.log("readyState is 4");
             if(this.status === 200) {
                 // The request was successful!
-                displayPlace(this.responseText);
+                displayResponse(this.responseText);
+                console.log("200");
             } else if (this.status === 404){
-                // No postal code found
-                displayPlace('{ "country" : "none" }');
+                // No holidays found
+                displayholidays('{ "country" : "none" }');
+                console.log("404");
             } else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
@@ -23,27 +30,30 @@ function findZip(zipId) {
             // Waiting for a response...
         }
     };
-    // Notice how the URL is appended with the zip code
-    var url = "http://api.zippopotam.us/us/" + zip;
+    // URL appended with myCountry, myYear and myMonth
+    //TEST   30d78ea8-80f8-49e0-94e2-f32757b8ee32
+    //       24957801-683a-498b-bb01-d6ef7fb876e4
+    var url = "https://holidayapi.com/v1/holidays?key=24957801-683a-498b-bb01-d6ef7fb876e4&country=" + myCountry + "&year=" + myYear + "&month=" + myMonth;
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
 /**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
+ * Displays the myCountry code holidays given the JSON data
+ * @param {string} data JSON data representing holidays for given myCountry code
  */
-function displayPlace(data){
-    var place = JSON.parse(data);
-    if(place.country === "none") {
-        document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "No place matches that zip code."
+function displayResponse(data){
+    var selectedHolidays = JSON.parse(data);
+    var chosenHolidays = [];
+    console.log(selectedHolidays);
+    if(selectedHolidays.holidays.length == 0) {
+        document.getElementById("selectedHolidays").className = "alert alert-warning";
+        document.getElementById("selectedHolidays").innerHTML = "No holidays found."
     } else {
-        document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = place.places[0]["place name"] +
-        ", " +
-        place.places[0].state +
-        ", " +
-        place.country;
+        document.getElementById("selectedHolidays").className = "alert alert-success";
+        for(i = 0; i < selectedHolidays.holidays.length; i++) {
+            chosenHolidays.push(selectedHolidays.holidays[i].name + ":" + selectedHolidays.holidays[i].date + "<br>");
+        }
+        document.getElementById("selectedHolidays").innerHTML = chosenHolidays;;
     }
 }
